19d8fbb330421f62faf76e0c74d758bb88c02923
O6SPIDER <arnoldagyemang14@gmail.com>
2025-08-25 01:03:59 +0300

    favicon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,10 +9,6 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
       {/* Add meta viewport for mobile responsiveness */}
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Spiderwebs Agency</title>
-      </head>
 
       {/* Header */}
       <Header />
